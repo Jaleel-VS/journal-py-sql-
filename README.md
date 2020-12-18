85bf627f58de0195254e20996193f2802cc759df
Jaleel-VS <jeremikress@gmail.com>
2020-12-18 07:47:34 +0200

    Stored input in lists, learned about SQL

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# journal-py-sql-
+### Author: JD van Staden
+### Project: Programming journal/Accountability tracker with Python and SQL
+
+Progress:
+- 18/12/2018: 
+    - Create menu outline
---
+++ app.py
@@ -0,0 +1,26 @@
+from time import sleep
+from database import add_entry, view_entries
+
+menu = """\nPlease select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection:  """
+
+welcome = "Welcome to the programming diary"
+thank_you = "Have a nice day!"
+
+
+print(welcome)
+sleep(1.5)
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        add_entry()
+    elif user_input == "2":
+        view_entries()
+    else:
+        print("Invalid option, please try again!")
+    sleep(1.5)
+
+print(thank_you)
